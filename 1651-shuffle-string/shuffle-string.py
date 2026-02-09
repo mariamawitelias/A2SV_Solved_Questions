d5f4b6bfa33e953fec18695a11861e295d5cf9fe
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-02-09 17:30:57 +0300

    Time: 2 ms (36.00%) | Memory: 19.4 MB (37.64%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        table = {}
+        for x, y in zip(s, indices):
+            table[y] = x
+        print(table)
+        res = ""
+        for i in range(len(s)):
+            res += table[i]
+        return res
