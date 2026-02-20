4a7fdc3a4d734a0733acae948c4afc49d2625385
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-02-20 13:52:14 +0300

    Time: 2 ms (94.61%) | Memory: 23.6 MB (16.00%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        x = int(len(nums) / 3)
         count = Counter(nums)
+        a = len(nums) / 3
         res = []
-        for key, value in count.items():
-            if value > x:
-                res.append(key)
+        for k, v in count.items():
+            if v > a:
+                res.append(k)
         return res
+
 
 
 
