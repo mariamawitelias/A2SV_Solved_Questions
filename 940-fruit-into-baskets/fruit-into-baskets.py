13411d5ce5184a3be9b4b98ec07c392c2cd6db0b
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-02-26 00:08:27 +0300

    Time: 191 ms (54.46%) | Memory: 26 MB (28.22%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        window = {}
+        res = 0
+        l = 0
+        for r in range(len(fruits)):
+            window[fruits[r]] = window.get(fruits[r], 0) + 1
+            while len(window) > 2:
+                window[fruits[l]] -= 1
+                if window[fruits[l]] == 0:
+                    del window[fruits[l]]   
+                l += 1
+            res = max(res, (r - l + 1))
+        return res
