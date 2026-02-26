43ded2e361c78599c6f9757d34e74aac4dbbe911
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-02-26 16:11:46 +0300

    Implement segment counting with small sum logic

---
+++ Number-of-segments-with-small-sum.py
@@ -0,0 +1,12 @@
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+window = 0
+res = 0
+l = 0
+for r in range(n):
+    window += a[r]
+    while window > s:
+        window -= a[l]
+        l += 1
+    res += (r-l+1)
+print(res)
