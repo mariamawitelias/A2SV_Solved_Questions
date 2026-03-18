8cdc1ca7cd789d7e4c10f878fc7446a1e1b6bbdf
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-03-18 19:24:12 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.28%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        def recur(n, k):
+            if n == 1:
+                return 0
+            return (recur(n-1, k) + k) % n
+        return recur(n, k) + 1 
