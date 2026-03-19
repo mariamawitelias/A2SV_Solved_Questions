acf44c1873e2e153394edc694596c8721b6f2b8e
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-03-19 16:47:45 +0300

    Time: 0 ms (100.00%) | Memory: 21.8 MB (98.92%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderSuccessor(self, node):
+        while node.left is not None:
+            node = node.left
+        return node
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root is None:
+            return None 
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.right:
+                return root.left
+            elif not root.left:
+                return root.right
+            succ = self.inorderSuccessor(root.right)
+            root.val = succ.val
+            root.right = self.deleteNode(root.right, succ.val)
+        return root
+
+            
+
+
+            
