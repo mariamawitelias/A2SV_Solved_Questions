5067f59537321dec54f3645155d02d767962ed77
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-03-27 17:45:27 +0300

    Add solution for Sort List

---
+++ leetcode/0148-sort-list/0148-sort-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        fast, slow = head.next, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        return self.merge(left, right)
+    def merge(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        head = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next = l1
+                l1 = l1.next
+            else:
+                head.next = l2
+                l2 = l2.next
+            head = head.next
+        head.next = l1 if l1 else l2
+        return dummy.next
