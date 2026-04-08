48b447bc36c6d2d8cdc8096202c463ba1ea12bdc
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-04-08 18:14:42 +0300

    Add solution for Count of Smaller Numbers After Self

--- leetcode/0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ leetcode/0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,18 +1,37 @@
 class Solution:
     def countSmaller(self, nums):
         ans = [0] * len(nums)
-        def mergesort(l, r):
-            if l == r:
-                return [[nums[l], l]]
-            mid = (l + r) // 2
-            left = mergesort(l, mid)
-            right = mergesort(mid + 1, r)
+        def merge(left, right):
+            res = []
+            l, r = 0, 0
+            while l < len(left) and r < len(right):
+                if left[l][0] <= right[r][0]:
+                    res.append(left[l])
+                    l += 1
+                else:
+                    res.append(right[r])
+                    r += 1
+            res.extend(left[l:])
+            res.extend(right[r:])
+            return res
+
+        def calc(left, right):
             tmp = [num for num, idx in right]
             for num, idx in left:
                 i = bisect_left(tmp, num)
                 ans[idx] += i
-            return sorted(left + right)
-        mergesort(0, len(nums) - 1)
+
+        
+        def divide(l, r):
+            if l == r:
+                return [[nums[l], l]]
+            mid = (l + r) // 2
+            left = divide(l, mid)
+            right = divide(mid + 1, r)
+            calc(left, right)
+            return merge(left, right)
+        
+        divide(0, len(nums) - 1)
         return ans
 
 
