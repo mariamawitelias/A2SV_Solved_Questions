e8c429387539a3ff8c8c9032a7093eb07a425891
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-04-18 20:59:18 +0300

    Add solution for Maximum Gap

--- leetcode/0164-maximum-gap/0164-maximum-gap.py
+++ leetcode/0164-maximum-gap/0164-maximum-gap.py
@@ -1,8 +1,30 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        nums.sort()
-        res = 0
-        for i in range(len(nums)):
-            res = max(res, nums[i] - nums[i-1])
-        return res
+        n = len(nums)
+        if n < 2:
+            return 0    
+        minVal = min(nums)
+        maxVal = max(nums)
+        if minVal == maxVal:
+            return 0
+        bucketSize = max(1, (maxVal - minVal) // (n - 1))
+        bucketCount = (maxVal - minVal) // bucketSize + 1
+        buckets = [[None, None] for _ in range(bucketCount)]
+        for num in nums:
+            bucket = (num - minVal) // bucketSize
+            if buckets[bucket][0] is None:
+                buckets[bucket][0] = num
+                buckets[bucket][1] = num
+            else:
+                buckets[bucket][0] = min(buckets[bucket][0], num)
+                buckets[bucket][1] = max(buckets[bucket][1], num)
+        prevMax = None
+        maxGap = 0
+        for currMin, currMax in buckets:
+            if currMin is None:
+                continue
+            if prevMax is not None:
+                maxGap = max(maxGap, currMin - prevMax)
+            prevMax = currMax     
+        return maxGap
         
