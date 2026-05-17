c008ea28725598fc2a686fbc877d2889f407fd1b
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-05-17 23:36:37 +0300

    Add solution for Reverse Integer

---
+++ leetcode/0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sum = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            rem = x % 10
+            sum = sum * 10 + rem
+            x = x // 10
+
+        sum *= sign
+        
+        if sum > 2**31 - 1 or sum < -2**31:
+            return 0
+
+        return sum
