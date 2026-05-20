32d10d6b9de6656208d6fb12953588243da6b175
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2026-05-20 19:31:39 +0300

    Add solution for Target Sum

---
+++ leetcode/0494-target-sum/0494-target-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        def dp(i, res):
+            if i == len(nums):
+                if res == target:
+                    return 1
+                else:
+                    return 0
+            if (i, res) not in memo:
+                memo[(i, res)] = dp(i+1, res-nums[i]) + dp(i+1, res+nums[i])
+            return memo[(i, res)]
+        return dp(0, 0)
